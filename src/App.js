63a7d9b361837d160782345cd243e444d488bdbf
Diego <diegoadq@hotmail.com>
2019-05-31 13:36:56 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import React from 'react';
-
-import './App.css';
+import React from "react";
+import "./App.css";
 import Home from "./pages/Home";
 import Error from "./pages/Error";
 import SingleRoom from "./pages/SingleRoom";
 import Rooms from "./pages/Rooms";
-import Navbar from './components/Navbar';
-import {Route, Switch} from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import { Route, Switch } from "react-router-dom";
 
 // pagina de react-router-dom and netlify https://www.slightedgecoder.com/2018/12/18/page-not-found-on-netlify-with-react-router/
 //  pagina de documentacion de react-router-dom https://reacttraining.com/react-router/web/guides/quick-start
-// lo importe en el index.js 
+// lo importe en el index.js
 function App() {
   return (
     <>
-    <Navbar/>
-    <Switch>
-    <Route  exact path="/" component={Home}></Route> 
-    <Route exact path="/rooms/" component={Rooms}></Route>
-    <Route exact path="/rooms/:nombre" component={SingleRoom}></Route>
-    <Route component={Error}/>
-    </Switch>
-    
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:nombre" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
