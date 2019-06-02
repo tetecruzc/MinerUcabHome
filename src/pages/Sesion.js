ad04a1fa67c02827243ead2c47ecdf0f2ff7ed08
michialleb <alleyne.michelle@hotmail.com>
2019-06-02 16:10:48 -0400

    ...

--- src/pages/Sesion.js
+++ src/pages/Sesion.js
@@ -1,8 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
 import { HashRouter as Router, Route, Link, NavLink } from "react-router-dom";
-=======
->>>>>>> Tete
 import Menu from "../components/Menu";
 import Empleado from "./Empleado";
 const Sesion = () => {
