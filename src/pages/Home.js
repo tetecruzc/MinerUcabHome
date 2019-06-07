6e6434dd5ccee1cfbc3dddd71d53bb1901fe0520
tetecruzc <stephaniecruzcastelli@hotmail.com>
2019-06-07 14:34:14 -0400

    menu + creates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,17 +7,6 @@ import Minerales from "../components/Minerales";
 const Home = () => {
   return (
     <>
-<<<<<<< HEAD
-<Hero hero="defaultHero">
-    <Banner banner="banner" title="Miner Ucab" subtitle="Loren ipsum dolor no se que poner aca aun ">
-        <Link to="/nosotros" className="btn-primary">Nosotros</Link> 
-    </Banner>
-</Hero>
-<Services/>
-<Minerales/>
-</>
-)};
-=======
       <Hero hero="defaultHero">
         <Banner
           banner="banner"
@@ -34,6 +23,5 @@ const Home = () => {
     </>
   );
 };
->>>>>>> michialleb
 
 export default Home;
