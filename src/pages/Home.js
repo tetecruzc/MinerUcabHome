7d65ad2e2dac83817b5b6d39254939345951a79e
michialleb <alleyne.michelle@hotmail.com>
2019-06-07 14:21:04 -0400

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,27 @@
 import React from "react";
-import Hero from '../components/Hero';
-import Banner from '../components/Banner';
-import {Link} from 'react-router-dom';
-import Services from '../components/Services';
-import Minerales from '../components/Minerales';
-const Home =() => {
-return (
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import Services from "../components/Services";
+import Minerales from "../components/Minerales";
+const Home = () => {
+  return (
     <>
-<Hero hero="defaultHero">
-    <Banner banner="banner" title="Miner Ucab" subtitle="Loren ipsum dolor no se que poner aca aun ">
-        <Link to="/rooms" className="btn-primary">Nosotros</Link> 
-    </Banner>
-</Hero>
-<Services/>
-<Minerales/>
-</>
-)};
+      <Hero hero="defaultHero">
+        <Banner
+          banner="banner"
+          title="Miner Ucab"
+          subtitle="Loren ipsum dolor no se que poner aca aun "
+        >
+          <Link to="/nosotros" className="btn-primary">
+            Nosotros
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+      <Minerales />
+    </>
+  );
+};
 
 export default Home;
